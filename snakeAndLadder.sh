abd8ccf5aa0b2c5db1f02fe58888f637c892881a
paras <parasjitaliya@gmail.com>
2020-02-07 12:37:32 +0530

    added:initialize player position

---
+++ snakeAndLadder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "-------------welcome to new game------------------"
+echo "..........snake on position 16-5 and 37-21............"
+echo "..........ladder on position 4-17 and 23-39..........."
+#ASSIGN VALUE FOR VARIABLE
+player_start_pos=0
