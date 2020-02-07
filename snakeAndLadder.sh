7540531f1b14a565531761fc064e1313e76b207e
paras <parasjitaliya@gmail.com>
2020-02-07 13:57:34 +0530

    added:check option for snake and ladder

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,3 +5,17 @@ echo "..........ladder on position 4-17 and 23-39..........."
 #ASSIGN VALUE FOR VARIABLE
 player_start_pos=0
 rolldie=$((RANDOM%6+1))
+function checkOptions(){
+options=$((RANDOM%3))
+if [ $options -eq 0 ]
+then
+	pos=$player1_start_pos
+elif [ $options -eq 1 ]
+then 
+	pos=$(($pos+$rolldie))
+else
+	pos=$(($pos-$rolldie))
+fi
+}
+ 
+checkOptions
