b213d1f9074b9d121cfc3b0950337c3523525fbe
paras <parasjitaliya@gmail.com>
2020-02-07 14:20:30 +0530

    Added:condition for rich win position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,17 +5,24 @@ echo "..........ladder on position 4-17 and 23-39..........."
 #ASSIGN VALUE FOR VARIABLE
 player_start_pos=0
 rolldie=$((RANDOM%6+1))
-function checkOptions(){
-options=$((RANDOM%3))
-if [ $options -eq 0 ]
-then
-	pos=$player1_start_pos
-elif [ $options -eq 1 ]
-then 
-	pos=$(($pos+$rolldie))
-else
-	pos=$(($pos-$rolldie))
-fi
-}
- 
+function checkOptions()
+{
+	options=$((RANDOM%3))
+	while [ $pos -le $WIN_POS ]
+	do
+		if [ $options -eq 0 ]
+		then
+			pos=$PLAYER1_START_POS
+		elif [ $options -eq 2 ]
+		then 
+			pos=$(($pos+$rolldie))
+		else
+			pos=$(($pos-$rolldie))
+			if [ $pos -lt 0 ]
+			then 
+				pos=$PLAYER1_START_POS
+			fi
+		fi
+	done
+} 
 checkOptions
