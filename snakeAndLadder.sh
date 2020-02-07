d5341768201fc2ee538a6a90f05afd9c2d2058bd
paras <parasjitaliya@gmail.com>
2020-02-07 13:50:19 +0530

    added:Roll

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,3 +4,4 @@ echo "..........snake on position 16-5 and 37-21............"
 echo "..........ladder on position 4-17 and 23-39..........."
 #ASSIGN VALUE FOR VARIABLE
 player_start_pos=0
+rolldie=$((RANDOM%6+1))
