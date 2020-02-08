e456608f4e66fc6b4bf2665b58f6c38a09503bd6
paras <parasjitaliya@gmail.com>
2020-02-08 12:28:36 +0530

    Added:player 2 added

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -8,8 +8,7 @@ pos=$player_start_pos
 checkvalue=1
 totalrolldie=0
 function roll()
-{
-	
+{	
 	rolldie=$((RANDOM%6+1))
 	totalrolldie=$((totalrolldie+1))
 	echo $rolldie ..this trun
@@ -18,23 +17,23 @@ function checkOptions()
 {
 	options=$((RANDOM%3))
 	case $options in
-			  1)
-					pos=$pos 
-					;;
-			  2)
-					pos=$(( $pos + $rolldie ))
-			    	if [ $pos -gt $win_pos ]
-  					then
-     					pos=$(( $pos - $rolldie ))
-  					fi
-					;;
-				3)
-					pos=$(( $pos - $rolldie ))
-					if [ $pos -lt $win_pos ]
-  					then
-     				   pos=$player_start_pos
-  					fi
-					;;
+		1)
+			pos=$pos 
+			;;
+		2)
+			pos=$(( $pos + $rolldie ))
+			if [ $pos -gt $win_pos ]
+  			then
+     				pos=$(( $pos - $rolldie ))
+  			fi
+			;;
+		3)
+			pos=$(( $pos - $rolldie ))
+			if [ $pos -lt $win_pos ]
+  			then
+     				pos=$player_start_pos
+  			fi
+			;;
 	esac
 	echo $pos
 }
