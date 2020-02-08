8274cfb75364990126297de6c49475c1e1ed9bc5
paras <parasjitaliya@gmail.com>
2020-02-08 12:10:39 +0530

    Added:Ensure position and commenting

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -6,24 +6,29 @@ win_pos=100
 pos=$player_start_pos
 rolldie=$((RANDOM%6+1))
 #function for chaking operation
-function checkOptions(){
-options=$((RANDOM%3 + 1))
-   case $options in
-           1)pos=$pos ;;
-           2)pos=$(( $pos + $rolldie ))
-                   if [ $pos -gt $win_pos ]
-                   then
-                     pos=$(($pos-$randomCheck ))
-                   fi
-                  ;;
-           3)pos=$(( $pos - $rolldie ))
-                  if [ $pos -lt $player_start_pos ]
-                  then
-                     pos=$player_start_pos
-                  fi
-                  ;;
-   esac
-	echo $pos	
+function checkOptions()
+{
+	options=$((RANDOM%3 + 1))
+	case $options in
+		1)
+			pos=$pos 
+			;;
+		2)	
+			pos=$(( $pos + $rolldie ))
+			if [ $pos -gt $win_pos ]
+			then
+     				pos=$(( $pos - $randomCheck ))
+  			fi
+		  	;;
+		3)
+			pos=$(( $pos - $rolldie ))
+			if [ $pos -lt $win_pos ]
+  			then
+     				pos=$player_start_pos
+  			fi
+			;;
+	esac
+   	echo $pos	
 } 
 while [ $pos -lt $win_pos ]
 do
